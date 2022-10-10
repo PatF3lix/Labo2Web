f24fb5ceb344a5e9f52e5d1853bf862f30bea2e7
PatF3lix <pat_pg@hotmail.fr>
2022-10-10 00:57:06 -0400

    stacking the cards in a deck

--- script.js
+++ script.js
@@ -19,21 +19,24 @@ const SUITS = [
         color: 'black',
     }
 ];
-const VALUES = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-const mycardset = []
+
+const SPACING = 10;
+const VALUES = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+const mycardset = [];
 
 const container = document.getElementById('container');
+const buttonshuffle = document.getElementById('shuffle')
 
-function createCard(value, suit) {
-    const card = document.createElement
-    ('div');
-
+function createCard({value, suit, index_suit, index_value}) {
+    const card = document.createElement('div');
     card.classList.add('card');
 
     if(suit.color == 'red') {
         card.classList.add('red');
-    }
+    }else{card.classList.add('black')}
 
+    card.style.top = index_suit * 150 + SPACING * index_suit +'px';
+    card.style.left = index_value * 100 + SPACING * index_value + 'px';
     card.innerHTML = `
         <span class="number top">${value}</span>
         <p class="suit">${suit.icon}</p>
@@ -42,42 +45,30 @@ function createCard(value, suit) {
 }
 
 // Creating the cards
-SUITS.forEach(suit => {
-    VALUES.forEach(value => {
-        createCard(value, suit)
+SUITS.forEach((suit, index_suit) => {
+    VALUES.forEach((value, index_value) => {
+        const cardDetails = {
+            value,
+            suit,
+            index_suit,
+            index_value
+        }
+        createCard(cardDetails);
     });
 });
 
+buttonshuffle.addEventListener('click', () => {
+    const cards = document.querySelectorAll('.card');
 
+    cards.forEach((card, index) => {
+        setTimeout( () => {
+            card.styleZindex = 52 - index
+            card.style.top = '50%';
+            card.style.left = '50%';
+        }, index * 50);
+    });
+});
 
-
-
-
-
-
-
-
-
-// class Card{
-//     constructor(suite, value) {
-//         this.suite = suite
-//         this.value = value
-//     }
-// }
-
-// class Deck{
-//     constructor() {
-//         const half = length(VALUES)*2
-//         const SUITE = ['♦','♣','♥','♠']
-//         const VALUES = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-//         const mycardset = []
-
-//         for (var indexSuite = 0; indexSuite < 4; indexSuite++) {
-//             for (var indexValues = 0; indexValues < half; indexValues++){
-//                 mycardset.push(new Card(SUITE[indexSuite] + VALUES[indexValues]));
-//             } 
-//         }
-//     }
 
 //     shuffling() {
 //         halfDeck1 = this.mycardset.slice(0, this.half)
@@ -106,5 +97,3 @@ SUITS.forEach(suit => {
 //     }
     
 // }
-
-// let deck = new Deck()
